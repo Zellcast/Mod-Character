19f7320b7346b1bedc4005ca01ba27c030197f8e
Zellcast <rollspinner@7tekzen.work>
2025-01-09 04:19:42 +0000

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,3 +1,27 @@
+import { defineStackbitConfig } from "@stackbit/types";
+import { GitContentSource } from "@stackbit/cms-git";
+
+export default defineStackbitConfig({
+  // ...
+  contentSources: [
+    new GitContentSource({
+      rootPath: __dirname,
+      contentDirs: ["content"],
+      models: [
+        {
+          name: "Page",
+          // Define the model as a page model
+          type: "page",
+          urlPath: "/{slug}",
+          filePath: "content/pages/{slug}.json",
+          fields: [{ name: "title", type: "string", required: true }]
+        }
+      ],
+    })
+  ]
+});
+
+
 import { defineStackbitConfig } from "@stackbit/types";// stackbit.config.ts
 import { defineStackbitConfig, SiteMapEntry } from "@stackbit/types";
 
